8d883aea8aad0dd961bd43cbdf7c237a07dfc7d8
wonyul <jwsuh25@kis.ac>
2022-11-27 19:24:54 +0300

    Added threading

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests
 from bs4 import BeautifulSoup as bs
+from multiprocessing.pool import ThreadPool as Executor
+import re
 
-keyword = "이혼"
+keyword = "미친"
+MAX_WORKERS = 100
 
 class BC:
     HEADER = '\033[95m'
@@ -14,31 +17,59 @@ class BC:
     UNDERLINE = '\033[4m'
     RESET = '\033[0m'
 
+def get_sillok(url):
+    try:
+        response = requests.get(url)
 
-with open('result.txt') as f:
+        soup = bs(response.text, 'html.parser')
+        soup.sup.clear()
+        paragraph = soup.find('div', {'class': 'ins_view_pd'})
+        paragraph = paragraph.find_all('p', {'class': 'paragraph'})
 
+        final = ''
+        for p in paragraph:
+            final += p.text
 
+        final = final.replace('              ', ' ').replace('        ', ' ').replace('  ', ' ')
 
-    # print each line
-    for line in f:
-        url = line.split('^')[4].replace('http://', 'https://').replace('\n', '')
-        if url.startswith('https://'):
+        # python remove characters inside brackets
+        final = re.sub(r'\([^)]*\)', '', final)
+        final = re.sub(r'\[[^)]*\]', '', final)
+        prev = ''
 
-            response = requests.get(url)
+        for sentence in final.split('. '):
+            if keyword in sentence:
 
-            soup = bs(response.text, 'html.parser')
-            paragraph = soup.find('div', {'class': 'ins_view_pd'})
-            paragraph = paragraph.find_all('p', {'class': 'paragraph'})
+                if prev != '':
+                    longer = f'{prev}. {sentence}.'
+                else:
+                    longer = sentence
 
-            final = ''
-            for p in paragraph:
-                final += p.text
-
-            final = final.replace('              ', ' ')
-
-            for sentence in (final.split('. ')):
-                if keyword in sentence:
-                    print(url)
-                    print(sentence.replace(keyword, BC.BOLD + BC.OKGREEN + keyword + BC.RESET))
-                    print('')
+                if longer == '':
+                    continue
+                elif len(longer.encode("UTF-8")) <= 280:
+                    print(longer.replace(keyword, BC.BOLD + BC.OKGREEN + keyword + BC.RESET))
                     break
+
+                elif len(sentence.encode("UTF-8")) <= 280:
+                    print(sentence.replace(keyword, BC.BOLD + BC.OKGREEN + keyword + BC.RESET) + '.')
+                    break
+            prev = sentence
+    except:
+        pass
+
+
+
+if __name__ == "__main__":
+    with open('result.txt') as f:
+        urls = []
+        for line in f:
+            url = line.split('^')[4].replace('http://', 'https://').replace('\n', '')
+            if url.startswith('https://'):
+                urls.append(url)
+
+        pool_size = min(len(urls), MAX_WORKERS)
+        with Executor(pool_size) as executor:
+            executor.map(get_sillok, urls)
+
+    print(f'{BC.OKGREEN}Done.{BC.RESET}')
